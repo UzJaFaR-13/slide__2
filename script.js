1dcf3fba5207afaaf211919e255006e073b86e0d
UzJaFaR-13 <88071148+UzJaFaR-13@users.noreply.github.com>
2021-09-12 03:14:08 +0500

    52. Background-Image-slider
    52. Background-Image-slider

---
+++ script.js
@@ -0,0 +1,44 @@
+// coding with nick
+
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+  activeSlide++
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0
+  }
+
+  setBgToBody()
+  setActiveSlides()
+})
+
+
+leftBtn.addEventListener('click', () => {
+  activeSlide--
+
+  if (activeSlide < 0 ) {
+    activeSlide = slides.length - 1
+  }
+
+  setBgToBody()
+  setActiveSlides()
+})
+
+setBgToBody()
+
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlides() {
+  slides.forEach((slide) => slide.classList.remove('active'))
+
+  slides[activeSlide].classList.add('active')
+}
